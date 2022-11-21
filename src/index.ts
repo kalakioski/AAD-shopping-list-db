f4a4596f36c6015d2e8454c8db4fc89adac64147
kalakioski <71070273+kalakioski@users.noreply.github.com>
2022-11-21 19:01:43 +0200

    Working base
    - Encrypts passwords
    - Generates JWTs from login
    - Verifies password
    - Create a new user
    - Security through private/public keys

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,65 @@
-import "reflect-metadata"
-import { buildSchema } from "type-graphql"
-import express from "express"
-import { graphqlHTTP } from "express-graphql"
+import 'reflect-metadata';
+import dotenv from 'dotenv';
+import express from 'express';
+import { buildSchema } from 'type-graphql';
+import cookieParser from 'cookie-parser';
+import { ApolloServer } from 'apollo-server-express';
+import {
+  ApolloServerPluginLandingPageProductionDefault,
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} from 'apollo-server-core';
+import { resolvers } from './resolvers';
+import { connectToMongo } from './utils/mongo';
+import { verifyJwt } from './utils/jwt';
+import { User } from './schema/user.schema';
+import Context from './types/context';
 
-import { UsersResolver } from "./users/users.resolvers"
+dotenv.config();
 
-async function main() {
-    const schema = await buildSchema({
-        resolvers: [UsersResolver],
-        emitSchemaFile: true,
-    })
+async function bootstrap() {
+  // Build the schema
 
-    const app = express()
+  const schema = await buildSchema({
+    resolvers,
+    // authChecker,
+  });
 
-    app.use(
-        "/graphql",
-        graphqlHTTP({
-            schema: schema,
-            graphiql: true,
-        })
-    )
+  // Init express
 
-    app.listen(8000)
+  const app = express();
 
-    console.log("Running a GraphQL API server at http://localhost:8000/graphql")
+  app.use(cookieParser());
+
+  // Create the apollo server
+  const server = new ApolloServer({
+    schema,
+    context: (ctx: Context) => {
+      const context = ctx;
+
+      if (ctx.req.cookies.accessToken) {
+        const user = verifyJwt<User>(ctx.req.cookies.accessToken);
+
+        context.user = user;
+      }
+      return context;
+    },
+    plugins: [
+      process.env.NODE_ENV === 'production'
+        ? ApolloServerPluginLandingPageProductionDefault()
+        : ApolloServerPluginLandingPageGraphQLPlayground(),
+    ],
+  });
+
+  await server.start();
+
+  // apply middleware to server
+  server.applyMiddleware({ app });
+
+  // app.listen on express server
+  app.listen({ port: 4000 }, () => {
+    console.log('App is listening on http://localhost:4000');
+  });
+  connectToMongo();
 }
 
-main()
+bootstrap();
---
+++ src/utils/jwt.ts
@@ -0,0 +1,25 @@
+import config from 'config';
+import jwt from 'jsonwebtoken';
+require('dotenv').config();
+
+const privatek = process.env.PRIVATE_KEY;
+const publick = process.env.PRIVATE_KEY;
+
+const publicKey = Buffer.from(<string>publick, 'base64').toString('ascii');
+const privateKey = Buffer.from(<string>privatek, 'base64').toString('ascii');
+
+export function signJwt(object: Object, options?: jwt.SignOptions | undefined) {
+  return jwt.sign(object, privateKey, {
+    ...(options && options),
+    algorithm: 'RS256',
+  });
+}
+
+export function verifyJwt<T>(token: string): T | null {
+  try {
+    const decoded = jwt.verify(token, publicKey) as T;
+    return decoded;
+  } catch (e) {
+    return null;
+  }
+}
