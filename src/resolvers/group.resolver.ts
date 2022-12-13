59ecd74698da4ae21023eeeaa97614198926c637
Jarkko Heinonen <71126476+JarkkoJake@users.noreply.github.com>
2022-12-13 15:19:20 +0200

    authentication checked when creating, deleting or managing group users

--- src/resolvers/group.resolver.ts
+++ src/resolvers/group.resolver.ts
@@ -1,8 +1,9 @@
-import { Arg, Mutation, Resolver } from 'type-graphql';
+import { ApolloError } from 'apollo-server';
+import { Arg, Ctx, Mutation, Resolver } from 'type-graphql';
 import { CreateGroupInput, Group, DeleteGroupInput, UserGroupIdInput } from '../schema/group.schema';
 import GroupService from '../service/group.service';
+import Context from '../types/context';
 
-/* TODO: authentication */
 @Resolver()
 export default class GroupResolver {
   constructor(private groupService: GroupService) {
@@ -11,25 +12,29 @@ export default class GroupResolver {
 
   /* Create new group */
   @Mutation(() => Group)
-  createGroup(@Arg('input') input: CreateGroupInput) {
-    return this.groupService.createGroup(input, "6395efc0dc0b97cf8335227c");
+  createGroup(@Arg('input') input: CreateGroupInput, @Ctx() context: Context) {
+    if (!context.user) throw new ApolloError("You need to be logged in to create groups");
+    return this.groupService.createGroup(input, context.user._id);
   }
 
   /* Remove a group with id */
   @Mutation(() => Boolean)
-  deleteGroup(@Arg('input') input: DeleteGroupInput) {
-    return this.groupService.deleteGroup(input);
+  deleteGroup(@Arg('input') input: DeleteGroupInput, @Ctx() context: Context) {
+    if (!context.user) throw new ApolloError("You need to be logged in to delete groups");
+    return this.groupService.deleteGroup(input, context.user._id);
   }
 
   /* Add user to the group */
   @Mutation(() => Group)
-  addUserToGroup(@Arg("input") input: UserGroupIdInput) {
-    return this.groupService.addUserToGroup(input);
+  addUserToGroup(@Arg("input") input: UserGroupIdInput, @Ctx() context: Context) {
+    if (!context.user) throw new ApolloError("You need to be logged in to edit groups");
+    return this.groupService.addUserToGroup(input, context.user._id);
   }
 
   /* Remove user from a group */
   @Mutation(() => Group)
-  removeUserFromGroup(@Arg("input") input: UserGroupIdInput) {
-    return this.groupService.removeUserFromGroup(input);
+  removeUserFromGroup(@Arg("input") input: UserGroupIdInput, @Ctx() context: Context) {
+    if (!context.user) throw new ApolloError("You need to be logged in to edit groups");
+    return this.groupService.removeUserFromGroup(input, context.user._id);
   }
 }
--- src/service/group.service.ts
+++ src/service/group.service.ts
@@ -3,22 +3,28 @@ import { CreateGroupInput, GroupModel, DeleteGroupInput, UserGroupIdInput } from
 import { User, UserModel } from '../schema/user.schema';
 import mongoose from 'mongoose';
 
+/* when managing group members, the id of the managed user is always in the input.
+The user id outside of input type always comes from context (current user) */
 class GroupService {
 
   /* Create group, adds the given used to the users field */
   async createGroup(input: CreateGroupInput, userId: User["_id"]) {
-    /* TODO: remove when authentication implemented */
-    const id = new mongoose.Types.ObjectId(userId);
-    return await GroupModel.create({...input, users: [id]});
+    return await GroupModel.create({...input, users: [new mongoose.Types.ObjectId(userId)]});
   }
 
   /* Delete group, return true if group was deleted, false otherwise */
-  async deleteGroup(input: DeleteGroupInput) {
+  async deleteGroup(input: DeleteGroupInput, userId: User["_id"]) {
+
+    /* Check for group and that the user is in the group */
+    const group = await GroupModel.findById(input._id);
+    if (!group) throw new ApolloError("Group not found");
+    if (!group.users.includes(userId)) throw new ApolloError("You can only delete your own groups");
+
     return !!await GroupModel.findByIdAndDelete(input._id);
   }
 
   /* Add user to group with group id and user id */
-  async addUserToGroup(input: UserGroupIdInput) {
+  async addUserToGroup(input: UserGroupIdInput, userId: User["_id"]) {
 
     /* Check for user */
     const user = await UserModel.findById(input.userId);
@@ -27,6 +33,7 @@ class GroupService {
     /* Check for group */
     const group = await GroupModel.findById(input.groupId);
     if (!group) throw new ApolloError("Group not found");
+    if (!group.users.includes(userId)) throw new ApolloError("You can only edit your own groups");
 
     /* Check if user already in the group */
     if (group.users.includes(user._id)) throw new ApolloError("User is already in the group");
@@ -42,7 +49,7 @@ class GroupService {
   }
 
   /* Remove user from group with user id and group id */
-  async removeUserFromGroup(input: UserGroupIdInput) {
+  async removeUserFromGroup(input: UserGroupIdInput, userId: User["_id"]) {
 
     /* Check for user */
     const user = await UserModel.findById(input.userId);
@@ -51,6 +58,7 @@ class GroupService {
     /* Check for group */
     const group = await GroupModel.findById(input.groupId);
     if (!group) throw new ApolloError("Group not found");
+    if (!group.users.includes(userId)) throw new ApolloError("You can only edit your own groups");
 
     /* Update group and return the updated version */
     await GroupModel.updateOne(
